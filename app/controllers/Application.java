50052ba57a510a1448d23acbc6ada77b2921578b
xfernandez <xfernandez@abiquo.com>
2011-11-17 08:55:42 +0100

    First version yabe tutorial

---
+++ app/controllers/Application.java
@@ -0,0 +1,71 @@
+package controllers;
+
+import java.util.List;
+
+import models.Post;
+import play.Play;
+import play.cache.Cache;
+import play.data.validation.Required;
+import play.libs.Codec;
+import play.libs.Images;
+import play.mvc.Before;
+import play.mvc.Controller;
+
+public class Application extends Controller
+{
+
+    @Before
+    static void addDefaults()
+    {
+        renderArgs.put("blogTitle", Play.configuration.getProperty("blog.title"));
+        renderArgs.put("blogBaseline", Play.configuration.getProperty("blog.baseline"));
+    }
+
+    public static void index()
+    {
+        Post frontPost = Post.find("order by postedAt desc").first();
+        List<Post> olderPosts = Post.find("order by postedAt desc").from(1).fetch(10);
+        render(frontPost, olderPosts);
+
+    }
+
+    public static void show(final Long id)
+    {
+        Post post = Post.findById(id);
+        String randomID = Codec.UUID();
+        render(post, randomID);
+
+    }
+
+    public static void postComment(final Long postId,
+        @Required(message = "Author is required") final String author,
+        @Required(message = "A message is required") final String content,
+        @Required(message = "Please type the code") final String code, final String randomID)
+    {
+        Post post = Post.findById(postId);
+        validation.equals(code, Cache.get(randomID)).message("Invalid code. Please type it again");
+        if (validation.hasErrors())
+        {
+            render("Application/show.html", post, randomID);
+        }
+        post.addComment(author, content);
+        flash.success("Thanks for posting %s", author);
+        Cache.delete(randomID);
+        show(postId);
+    }
+
+    public static void captcha(final String id)
+    {
+        Images.Captcha captcha = Images.captcha();
+        String code = captcha.getText("#E4EAFD");
+        Cache.set(id, code, "10mn");
+        renderBinary(captcha);
+    }
+
+    public static void listTagged(final String tag)
+    {
+        List<Post> posts = Post.findTaggedWith(tag);
+        render(tag, posts);
+    }
+
+}
---
+++ app/models/User.java
@@ -0,0 +1,44 @@
+package models;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import play.data.validation.Email;
+import play.data.validation.Required;
+import play.db.jpa.Model;
+
+@Entity
+@Table(name = "Sblog_user")
+public class User extends Model
+{
+
+    @Email
+    @Required
+    public String email;
+
+    @Required
+    public String password;
+
+    public String fullname;
+
+    public boolean isAdmin;
+
+    public User(final String email, final String password, final String fullname)
+    {
+        this.email = email;
+        this.password = password;
+        this.fullname = fullname;
+    }
+
+    public static User connect(final String email, final String password)
+    {
+        return find("byEmailAndPassword", email, password).first();
+    }
+
+    @Override
+    public String toString()
+    {
+        return email;
+    }
+
+}
